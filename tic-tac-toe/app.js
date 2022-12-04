0582b7098f8834b250357fb6ca6531348c726d67
Eliathx <eliath.velasco@gmail.com>
2022-12-04 00:44:28 -0500

    finished tic-tac-toe
    * finished GUI
    * draw and victory options
    * display text

--- tic-tac-toe/app.js
+++ tic-tac-toe/app.js
@@ -8,31 +8,56 @@ const winCombinations = [
     [0, 4, 8],
     [2, 4, 6]
 ]
-let firstTurn = true;
-let gameIsRunning = true;
+let firstTurn = true, gameIsRunning = true, winnerFound = false, clickCounter = 0;
 
 const checkWinCondition = () => {
     for (let i = 0; i < winCombinations.length; i++) {
-        let a = document.getElementById(`${winCombinations[i][0]}`).innerText
-        let b = document.getElementById(`${winCombinations[i][1]}`).innerText
-        let c = document.getElementById(`${winCombinations[i][2]}`).innerText
+        let a = document.getElementById(`${winCombinations[i][0]}`)
+        let b = document.getElementById(`${winCombinations[i][1]}`)
+        let c = document.getElementById(`${winCombinations[i][2]}`)
 
-        if (a === b && a === c && (a === "x" || a === "o")) {
-            console.log("Winner");
+        if (a.innerText === b.innerText && a.innerText === c.innerText && (a.innerText === "X" || a.innerText === "O")) {
+            a.style.color = "#226b9f";
+            b.style.color = "#226b9f";
+            c.style.color = "#226b9f";
             gameIsRunning = false;
+            winnerFound = true;
+
         }
     }
 }
 const addText = (event) => {
-    if(event.target.innerText === ""){
-        event.target.innerText = firstTurn ? "x" : "o";
+    if (event.target.innerText === "") {
+        event.target.innerText = firstTurn ? "X" : "O";
+        clickCounter++;
         firstTurn = !firstTurn
     }
 }
+
 const handleClick = (event) => {
-    if (gameIsRunning){
+    let message = document.getElementById("game-message");
+    if (gameIsRunning) {
         addText(event)
         checkWinCondition()
     }
+    if (clickCounter === 9 && !winnerFound) {
+        message.innerHTML = "It's a <a id='draw'>draw!</a>";
+        gameIsRunning = false;
+    } else {
+        let turn = firstTurn ? "X" : "O"
+        message.innerHTML = winnerFound ? "We have a <a id='winner'>winner</a>!" : "Current turn: "+turn;
+    }
 
+}
+
+const reset = () => {
+    let message = document.getElementById("game-message");
+    gameIsRunning = true;
+    winnerFound = false;
+    clickCounter = 0;
+    for (let i = 0; i < 9; i++) {
+        document.getElementById(`${i}`).innerText = "";
+        document.getElementById(`${i}`).style.color = "rgba(255, 255, 255, 0.75)";
+    }
+    message.innerText = "Click to begin game!";
 }
